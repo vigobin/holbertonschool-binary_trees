a2713da76d70694933f6a797f19ec79c0b515913
vigobin <vgobin@outlook.com>
2022-12-15 13:56:37 -0800

    fixed betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,8 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node.
  * @parent: pointer to the node to insert the left-child in.
  * @value: the value to store in the new node.
- * Return:  pointer to the created node, or NULL on failure or if parent is NULL.
+ * Return: pointer to the created node, or
+ *  NULL on failure or if parent is NULL.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -12,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
@@ -24,6 +25,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->left = parent->left;
 	parent->left = new;
 	if (new->left)
-		new->left->parent =  new;
+		new->left->parent = new;
 	return (new);
 }
