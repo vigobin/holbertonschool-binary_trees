871f14a4b464243327e323460f62ee662da3a6b1
vigobin <vgobin@outlook.com>
2022-12-16 10:31:12 -0800

    Added file for task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
